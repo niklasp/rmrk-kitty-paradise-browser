bd13f82d928426fd73919da93d0a0268cb677fcf
Niklas Jurij Plessing <contact@niklasplessing.net>
2022-03-30 12:49:34 +0200

    init kitty

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,19 @@ module.exports = {
     title: ``,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
     __key: "images"
-  }]
+    }
+  ]
 };
